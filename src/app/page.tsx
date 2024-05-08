cd0d195e362be3a60020e93e1387bbb935a6cd74
Ryan Braun <rbraun04@gmail.com>
2024-05-08 15:28:37 -0700

    try agian

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,6 +69,12 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
+          <h2 className="mb-3 text-2xl font-semibold">
+            again{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
